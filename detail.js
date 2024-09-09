29741671bc0dbac0584fd763e2dc63f2bdc6f144
iziyeon <dlfvkfclfrn@gmail.com>
2024-09-09 14:50:39 +0900

    <Select 인풋 다루기>
    * 실습 : 셔츠를 누르면 사이즈 선택지가 나오게 만들기(+)
    
    - document.querySelectorAll('.form-select').value == '셔츠'를 조건으로 하여 해당 조건을 만족하는 경우
      form-hide라는 class를 지우도록 함
      * 시행되지 않음 -> addEventListener('input', function(){}) 을 실행해야 사용자가 선택할 때마다
        함수를 시행하도록 설정할 수 있음.
    - 사용자가 셔츠를 누를 때 마다, 사이즈 선택지가 나오도록 함(+)

--- detail.js
+++ detail.js
@@ -1,7 +1,6 @@
 let 탭버튼 = document.querySelectorAll('.tab-button')
 let 탭컨텐츠 = document.querySelectorAll('.tab-content')
 
-
 for(let i = 0 ; i < 탭버튼.length ; i++){
     탭버튼[i].addEventListener('click', function(){
         for(j=0; j < 탭버튼.length ; j++){
@@ -12,3 +11,15 @@ for(let i = 0 ; i < 탭버튼.length ; i++){
             탭컨텐츠[i].classList.add('show');
         });
 }
+
+var car2 = { name : '소나타', price : [50000, 3000, 4000] } 
+document.querySelector('.car-title').innerHTML = car2.name;
+document.querySelector('.car-price').innerHTML = car2.price[0]
+
+let 선택 = document.querySelectorAll('.form-select');
+선택[0].addEventListener('input', function(){
+    if( 선택[0].value == '셔츠'){
+        선택[1].classList.remove("form-hide");
+    }
+});
+
